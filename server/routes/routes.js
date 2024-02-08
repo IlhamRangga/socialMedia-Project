b9739ca51807d12f2e43ba78db1fa5575b7a8ba5
IlhamRangga <ilhamranggakurniawan0@gmail.com>
2024-02-08 21:37:38 +0700

    add upload image logic

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,4 +1,5 @@
 import authorization from "../middleware/middleware.js"
+import upload from "../utils/upload/upload.js"
 
 const routes = (app, controller) => {
     app.post("/register", controller.register)
