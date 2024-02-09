a0759a6cc765eda3713e23728703ca45d76cf2eb
IlhamRangga <ilhamranggakurniawan0@gmail.com>
2024-02-09 21:50:01 +0700

    add react.js as front-end

--- server/app.js
+++ server/app.js
@@ -15,6 +15,6 @@ const controller = new Controller(service)
 
 routes(app, controller)
 
-app.listen(3000, () => {
-    console.log("listening on port 3000")
+app.listen(3001, () => {
+    console.log("listening on port 3001")
 })
